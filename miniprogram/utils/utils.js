20da955a2c339ee78b7054e90473b3f2edd9d66c
故事胶片 <pianjiao@gmail.com>
2021-08-09 18:52:20 +0800

    增加使用方式

--- miniprogram/utils/utils.js
+++ miniprogram/utils/utils.js
@@ -1,7 +1,7 @@
 module.exports = {
   tempId: '模板ID', 
-  corpId: '企业微信应用corpId',
-  corpsecret: '企业微信corpsecret',
+  corpId: '企业微信corpId',
+  corpsecret: '企业微信应用corpsecret',
   appId: "小程序appid",
-  agentId: '企业微信agentId'
+  agentId: '企业微信应用agentId'
 }
