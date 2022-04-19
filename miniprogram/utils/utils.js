1405e232da4b636d5068fec8f707aa2644c2aab3
故事胶片 <pianjiao@gmail.com>
2022-04-19 09:53:45 +0800

    修复问题

--- miniprogram/utils/utils.js
+++ miniprogram/utils/utils.js
@@ -1,7 +1,7 @@
 module.exports = {
-  tempId: 'F6XgJdrbhLA_zrWddSCSHx0i78bSgxBNFStfA04r0Ho',
-  corpId: 'ww587e8298cb403cdb',
-  corpsecret: 'WDL-wXW1d_GdGn0qIXxyoclx6pHAkTuQens98EQ3ZZI',
-  appId: "wxaae48a78ed6066d8",
-  agentId: 1000003
+  tempId: 'F6XgJdrbhLA_zrWddSCSHx0i78bSgxBNFStfA04r0Ho', // 模板ID
+  corpId: 'ww587e8298cb403cdb', // 企业微信corpId
+  corpsecret: 'WDL-wXW1d_GdGn0qIXxyoclx6pHAkTuQens98EQ3ZZI', // 企业微信应用corpsecret
+  appId: "wxaae48a78ed6066d8", // 小程序appid
+  agentId: '1000003' // 企业微信应用agentId
 }
